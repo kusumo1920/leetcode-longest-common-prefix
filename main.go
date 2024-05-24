322f5008d253b18572a5b04bde9704b24fc15446
Kesit Budi Kusumo <kesit@kernel.id>
2024-05-24 23:12:38 +0700

    feat: add horizontal scanning solution
    O(n)

--- main.go
+++ main.go
@@ -1,5 +1,34 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	strs := []string{"flower", "flow", "flight"}
+	output := longestCommonPrefixSolution1(strs)
+	fmt.Println(output)
+}
+
+func longestCommonPrefixSolution1(strs []string) string {
+	result := ""
+
+	if len(strs) > 0 {
+		result = strs[0]
+	}
+
+	for i := 1; i < len(strs); i++ {
+		j := min(len(result), len(strs[i]))
+		for ; j > 0; j-- {
+			if result[:j] == strs[i][:j] {
+				result = result[:j]
+				break
+			}
+		}
+
+		if j == 0 {
+			result = ""
+			break
+		}
+	}
+
+	return result
 }
